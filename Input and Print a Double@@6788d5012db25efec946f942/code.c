34667b90bf6347f7257da55081f9147d8ab92a0e
Saksham Jain <saksham1382.becse24@chitkara.edu.in>
2025-03-12 20:20:55 +0530

    Solution to Input and Print a Double

---
+++ Input and Print a Double@@6788d5012db25efec946f942/code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    double num;
+
+    printf("Enter a double-precision floating-point number: ");
+    scanf("%lf", &num);
+
+    printf("You entered: %.4lf\n", num);
+
+    return 0;
+}
